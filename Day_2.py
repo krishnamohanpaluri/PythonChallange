da3255304c7841170f142d71d0fdb6d2def87cd7
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-09-28 23:59:57 +0530

    Create Day_2.py

---
+++ Day_2.py
@@ -0,0 +1,7 @@
+#Q_1: Write a program to convert the given integer to a positive integer
+integer = int(input("Enter your Integer: "))
+if integer<0:
+    print(integer*(-1))
+else:
+    print(integer)
+###################################
