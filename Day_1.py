30022b953120e096b33b8bd0a02653c4152064b2
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-09-28 23:47:37 +0530

    Create Day_1.py

---
+++ Day_1.py
@@ -0,0 +1,137 @@
+################################################################
+
+                    #Day1 of Python Challange#
+
+
+################################################################
+#x = input()
+#y = len(x)
+#z = int(y/2) 
+#print(z)
+#print(x[:z])
+############################################
+#W = input()
+#N = int(input())
+#print(W*N)
+############################################
+#W = input()
+#N = int(input())
+#X = len(W)
+#Y = X-3
+#print(W[Y:]*N)
+############################################
+##x = int(input())
+##y = int(x/100)
+#print(y)
+##z = int(x%100)
+#print(z)
+##a = int(z/10)
+#print(a)
+##b = int(z%10)
+#print(b)
+##print(y+a+b)
+###############################################
+#x = input("Enter First String: ")
+#y = input("Enter Second String: ")
+#if (x[:3]==y[:3]):
+#    print("true")
+#else:
+#    print("Flase")
+#################################################
+#x = int(input("Enter First Int: "))
+#y = int(input("Enter Second Int: "))
+#print(((x>0) and (y>0)) and ((x>5) or (y>5)))
+#################################################
+#Kelometers = float(input())
+#meters = int(Kelometers*1000)
+#print(meters)
+##################################################
+#string = input()
+#print(len(string))
+#################################################
+#string = input()
+#print(string[:3])
+##################################################
+#x = int(input("Enter your number: "))
+#y = x/10 #first digit
+#z = x%10 #second digit
+#result = x<25 and y<z
+#print(result)
+###################################################
+#string_1 = input()
+#string_2 = input()
+#result = string_1[-3:] == string_2[-3:]
+#print(result)
+###################################################
+
+#string = input("Enter your string: ")
+#x = len(string)
+#y = int(x/2)
+#print(string[y:])
+##################################################
+
+#string = input("Enter Your Sring: ")
+#N = int(input("Enter Your Number: "))
+#print((string+ " ")*N)
+#####################################################
+#N = int(input("Enter your four digit Number: "))
+#x = int(N/1000)
+#print("First Digit is: " + str(x))
+#y = N%1000
+#print("not the last Digit is: " + str(y))
+#z = y%500
+#print("not the last Digit is: " + str(z))
+#a = z%60
+#print("last Digit is: " + str(a))
+#######################################################
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#result1 = a>=10 and a<=20
+#result2 = b>=10 and b<=20
+#result3 = c>=10 and c<=20
+#final_result = result1 or result2 or result3
+#print(final_result)
+#########################################################
+#side_of_rectangle = int(input())
+#area_of_rectangle = side_of_rectangle*side_of_rectangle
+#perimeter_of_rectangle = 4*side_of_rectangle
+#print(area_of_rectangle)
+#print(perimeter_of_rectangle)
+##########################################################
+#N = int(input())
+#Y = int(N/365)
+#a = int(N%365)
+#b = int(a/7)
+#c = int(a%7)
+#print(Y)
+#print(a)
+#print(b)
+#print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
