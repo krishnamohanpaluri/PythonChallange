13a568f2f7e9dd4d6e696b307d90729bcdb48711
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-09-30 09:58:32 +0530

    Update Day_2.py

--- Day_2.py
+++ Day_2.py
@@ -1,7 +1,13 @@
 #Q_1: Write a program to convert the given integer to a positive integer
-integer = int(input("Enter your Integer: "))
-if integer<0:
-    print(integer*(-1))
+#integer = int(input("Enter your Integer: "))
+#if integer<0:
+#    print(integer*(-1))
+#else:
+#    print(integer)
+############Conditional Statments#######################
+#Q_2 Write a program to print "positive" if the input is positive and negative
+Number = int(input("Enter your Number: "))
+if Number>0:
+    print(str(Number)+" is a positive Number")
 else:
-    print(integer)
-###################################
+    print(str(Number)+" is a negative number")
