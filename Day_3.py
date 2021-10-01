8375f825f4aa7cebf13aa0d0969c4451efe15f7d
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-01 10:54:26 +0530

    Update Day_3.py

--- Day_3.py
+++ Day_3.py
@@ -1,2 +1,29 @@
 ###################DAY_3############################
 
+#CodingPractice_4: Q1: Write a program to check if the given number is even or odd.
+# number = int(input())
+# if number%2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+######################################################
+#Q2: Write a program to read a single line of input, which is a two-digit integer and
+# Print "Special Number" if any one of the following conditions are satisfied
+# 	1. The sum of digits is 7
+# 	2. One of the digits is 7
+# 	3. The number is a multiple of 7
+# Print "Normal Number" in all other cases
+# number = int(input())
+# a = int(number/10)
+# print(a)
+# b = number%10
+# print(b)
+
+# if (a+b==7) or ( a == 7 or b == 7) or (number%7==0):
+#     print("Special Number")
+# else:
+#     print("Normal Number")
+
+#########################################################
+#Q3: A number is called Special Eleven, if it is a multiple of 11 or if it is one more than a multiple of 11. 
+# Write a program to check if the given number is a Special Eleven number.
