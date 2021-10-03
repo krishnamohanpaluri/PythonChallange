52c51d9dd08d884ff2faa339b206ad5fd379d526
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-03 15:06:44 +0530

    Create Day_5.py

---
+++ Day_5.py
@@ -0,0 +1,38 @@
+#Coding Practice_Q5: Write a program to find season for the given month number.
+# month = int(input())
+# if (month == 11 or month ==12 or month == 1):
+#     print("Winter")
+# elif (month == 2 or month == 3):
+#     print("Spring")
+# elif (month == 4 or month ==5 or month == 6):
+#     print("Summer")
+# elif (month == 6 or month == 7):
+#     print("Rainy")
+# else:
+#     print("Autumn")
+
+###########################################################################
+# Q6: Given a student's percentage of attendance and status of having a medical report. 
+# Write a program to decide if a student is eligible to take the final exam.
+# A = input()
+# M = input()
+# B = len(A)
+# A = A[:(B-1)]
+# A = int(A)
+
+# if A>=75:
+#     print("Allowed to write the exam")
+# elif A<75 and M == "Y":
+#     print("Allowed to write the exam")
+# else:
+#     print("Cannot write the exam")
+###############################################################################
+
+
+
+
+
+
+
+
+
