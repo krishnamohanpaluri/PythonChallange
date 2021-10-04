d9c5ed02c67b454fd31242f1ccdf6ff7370019f2
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-04 17:32:44 +0530

    Update Day_6.py

--- Day_6.py
+++ Day_6.py
@@ -77,14 +77,65 @@
 #########################################################################
 # It was Raj's first day at school. His teacher Anu asked the students to meet every other student in the class and to introduce themselves. The teacher asked them to do handshakes when they meet each other.
 # If there are N number of students in the class then write a program to print the total number of handshakes made by the students.
+# n = int(input())
+# print(int(n*((n+1)/2))-n)
 
+########################################################################
 
+# The possible denominations of currency notes are 100, 50, 20 and 10. The amount A to be withdrawn is given as input.
+# Write a program to break the amount into minimum number of bank notes.
+# N = int(input())
+# a = int(N/100)
+# k = a*100
+#     #print(str(a)+ "*" + str(100))
+# print("100 Notes: "+str(a))
+# N = N - k
+#     #print(a)
+#     #print(N)
+# b = int(N/50)
+# l = b*50
+#     #print(str(b)+"*"+str(50))
+# print("50 Notes: "+str(b))
+# N = N-l
+#     #print(N)
+# c = int(N/20)
+# m = c*20
+#     #print(str(c)+"*"+str(20))
+# print("20 Notes: "+str(c))
+# N = N-m
+# d = int(N/10)
+# n = d*10
+#     #print(str(d)+"*"+str(10))
+# print("10 Notes: "+str(d))
 
+###################################################################
 
+# Write a program to find the minimum number of notes required for the amount M. 
+# Available note denominations are 2000, 500, 200, 50, 20, 5, 2, 1.
 
-
-
-
+N = int(input())
+a = int(N/2000)
+k = a*2000
+print(str(a)+ "*" + str(2000))
+#print("100 Notes: "+str(a))
+N = N - k
+     #print(a)
+     #print(N)
+b = int(N/500)
+l = b*500
+print(str(b)+"*"+str(500))
+#print("50 Notes: "+str(b))
+N = N-l
+     #print(N)
+c = int(N/200)
+m = c*200
+print(str(c)+"*"+str(200))
+#print("20 Notes: "+str(c))
+N = N-m
+d = int(N/50)
+n = d*50
+print(str(d)+"*"+str(50))
+#print("10 Notes: "+str(d))
 
 
 
