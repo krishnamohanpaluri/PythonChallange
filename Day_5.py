c1bf7ffbd19ebd90a85494653f567e1c5afe433f
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-04 11:36:13 +0530

    Day_6

--- Day_5.py
+++ Day_5.py
@@ -28,6 +28,68 @@
 #     print("Cannot write the exam")
 ###############################################################################
 
+#Q7:  In this problem, you need to write a program to calculate the electricity bill for a household, based on the units of electricity the household consumed. The price for unit varies based on the slab. The charges per unit for different slabs are as mentioned below:
+# For the first 50 units (0-50), the charge is 2/unit
+# For the next 100 units (51-150), the charge is 3/unit
+# For the next 100 units (151-250), the charge is 5/unit
+# For above 250 units (251 and above), the charge is 8/unit
+# Apart from these charges, there is also an additional surcharge of 20% on the total amount is added to the bill
+
+
+
+#Given an integer between 0 and 10000, write a program to print the sum of its digits.
+# number = int(input())
+# a = int(number /1000)
+# b = int(number%1000)
+# c = int(b/100)
+# d = int(b%100)
+# e = int(d/10)
+# f = int(d%10)
+# print(a+c+e+f)
+
+#Write a program to create a menu driven calculator that performs basic arithmetic operations (+, -, *, /, and %).
+# a = int(input())
+# b = int(input())
+# operator = input()
+# if operator == "+":
+#     print(a+b)
+# elif operator == "*":
+#     print(a*b)
+# elif operator == "-":
+#     print(a-b)
+# elif operator == "/":
+#     print(a/b)
+# elif operator == "%":
+#     print(a%b)
+
+#################################################################
+#Given an amount, write a program to find the minimum number of notes of different denominations that sum up to the given amount.
+#Available note denominations are 100, 50, 10, 1.
+##############################################################3
+# While loops##############
+
+n = int(input())
+
+
+counter = 0 
+while counter<n: 
+    row = "* "*n
+    print(row)
+    counter = counter+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Day_6.py
@@ -0,0 +1,46 @@
+# Given day number D as input, write a program to display the day name. 
+# (1 - Monday, 2 - Tuesday, 3 - Wednesday, 4 - Thursday, 5 - Friday, 6 - Saturday, 7 - Sunday)
+# D = int(input())
+
+# if D == 1:
+#     print("Monday")
+# elif D == 2:
+#     print("Tuesday")
+# elif D == 3:
+#     print("Wednesday")
+# elif D == 4:
+#     print("Thursday")
+# elif D == 5:
+#     print("Friday")
+# elif D == 6:
+#     print("Saturday")
+# else:
+#     print("Sunday")
+#####################################################################
+
+#Write a program to print the relation between two numbers, A and B
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
