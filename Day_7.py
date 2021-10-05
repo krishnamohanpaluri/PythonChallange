a89d3e4137b934b01d11e785d53be53f211d255d
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-05 17:19:51 +0530

    Update Day_7.py

--- Day_7.py
+++ Day_7.py
@@ -17,3 +17,34 @@
 
 ############################################################################
 
+# Write a program to find the minimum number of notes required for the amount M. Available note denominations are 2000, 500, 200, 50, 20, 5, 2, 1.
+
+# a = int(input())
+# b = int(a/2000)
+# a = a - b*2000
+# c = int(a/500)
+# a = a - c*500
+# d = int(a/200)
+# a = a - d*200
+# e = int(a/50)
+# a = a - e*50
+# f = int(a/20)
+# a = a - f*20
+# g = int(a/5)
+# a = a - g*5
+# h = int(a/2)
+# a = a - h*2
+
+# print("2000:"+str(b)+" "+ "500:"+str(c)+" "+"200:"+str(d)+" "+"50:"+str(e)+" "+"20:"+str(f)+" "+"5:"+str(g)+" "+"2:"+str(h)+" "+"1:"+str(a))
+
+
+# Write a program to find the minimum number of notes required for the amount M. Available note denominations are 500, 50, 10, 1.
+
+# a = int(input())
+# b = int(a/500)
+# a = a - b*500
+# c = int(a/50)
+# a = a - c*50
+# d = int(a/10)
+# a = a - d*10
+# print("500:"+str(b)+" "+"50:"+str(c)+" "+"10:"+str(d)+" "+"1:"+str(a))
