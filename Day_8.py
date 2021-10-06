dc3ed5d1dc23ae158e2e93192bdc5da89ae23f27
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-06 16:05:03 +0530

    Update Day_8.py

--- Day_8.py
+++ Day_8.py
@@ -26,7 +26,71 @@
 
 #Given two integers M and N, write a program to print a solid rectangle pattern of M rows and N columns using the asterisk character (*).
 #Note: Use For Loop
-n = int(input())
-m = int(input())
-for i in range(n, m ):
-    
+# r = int(input())
+# c = int(input())
+# for i in range(r):
+#     print("* "*c)
+
+###########################################################################
+
+# Given an integer number (N) as input. Write a program to print the right-angled triangular pattern of N lines using an asterisk(*) character.
+# Note: Use For Loop
+# n = int(input())
+# for i in range (1, n+1):
+#     print("* "*i)
+
+##########################################################################
+
+# Given an integer number (N) as input. Write a program to print the sum of first N natural numbers.
+# Note: Use For Loop
+# n =int(input())
+# total = 0
+# for i in range(n+1):
+#     total = total + i
+# print(total)
+
+#####################################################################
+
+# Given an integer N, write a program which reads N inputs and prints them.
+# Note: Use For Loop
+# n = int(input())
+# for i in range(n):
+#     k = int(input())
+#     print(k)
+
+####################################################################
+
+# Given an integer N, write a program which reads N inputs and prints the sum of the given input integers.
+# Note: Use For Loop
+# n = int(input())
+# total = 0
+# for i in range(n):
+#     k = int(input())
+#     total = total+k
+# print(total)
+
+# a = input()
+# len_of_a = len(a)
+# b = ""
+# for i in range(1, len_of_a):
+#     b = b + "-" + a[i]
+# print(a[0]+b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
