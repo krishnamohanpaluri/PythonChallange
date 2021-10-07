e93ae81236ab4478e94aa2e56e8ed7ca98493e9e
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-07 12:25:19 +0530

    Create Day_9.py

---
+++ Day_9.py
@@ -0,0 +1,160 @@
+#cp9_p5:
+# n = int(input())
+# m = int(input())
+# total = 0
+# for i in range (n, m+1):
+#     if i%2 == 0:
+#         total = total +i
+# print(total)
+
+###############################################
+
+#cp9_p6:
+# n = int(input())
+# m = int(input())
+# total = 0
+# for i in range (n, m+1):
+#     if i%2 == 1:
+#         total = total +i
+# print(total)
+
+#############################################
+
+#cp9_p7
+# t = int(input())
+# n = int(input())
+# m = int(input())
+# total = 0
+# for i in range(n, m+1):
+#     if i%t == 0:
+#         total = total+i
+# print(total)
+
+################################################
+
+#cp9_p8:
+
+# n = int(input())
+# total = 0
+# for i in range(n+1):
+#     total = total + i**2
+# print(total)
+
+################################################
+
+#cp9_p9:
+
+# n = int(input())
+# total = 0
+# for i in range(n+1):
+#     total = total + i**3
+# print(total)
+
+##############################################
+
+#cp9_p10:
+# total= 0
+# for i in range(10):
+#     n = int(input())
+#     total = total + n
+# print(total)
+# print(total/10)
+
+################################################
+
+#cp_p11
+
+# n = int(input())
+# n = str(n)
+# total = 0
+# for i in n:
+#     total = total+int(i)
+# print(total)
+
+###############################################
+##########Assignments-9#######################
+###############################################
+
+#a9_p1:
+
+# n = int(input())
+# m = int(input())
+# total = 1
+# for i in range(m):
+#     total =total*n
+# print(total)
+###############################################
+
+#a9_p2:
+# n = int(input())
+# m = int(input())
+# total = 0
+# for i in range(1, n+1):
+#     k =i**m
+# #    print(k)
+#     total = total + k
+# print(total)
+
+##############################################
+
+#a9_p3:
+
+# n = int(input())
+# m = int(input())
+# total =1 
+# for i in range(n, m+1):
+#     total = total*i
+# print(total)
+
+#################################################
+
+#a9_p4:
+n = int(input())
+for i in range(1, n+1):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
