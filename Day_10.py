cfea93efdfd500a7a2b6a6df82fe5dc326eba3d8
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-08 20:22:48 +0530

    Day_10

--- Day_10.py
+++ Day_10.py
@@ -17,4 +17,92 @@
 #     print(650+lastunits + (0.2*(650+lastunits)))
 
 ##################################################
-  
+#cp10_p3
+# string = input()
+# string = string.lower()
+# reverse_string = string[::-1]
+# reverse_string = reverse_string.lower()
+
+# if string == string[::-1] :
+#    print("Palindrome")
+# else:
+#     print("Not a Palindrome")
+
+##################################################
+#cp10_p4
+# no_of_inputs = int(input())
+# first_number = int(input())
+# greatest_number = first_number
+# for i in range(no_of_inputs-1):
+#     number = int(input()) 
+#     if number>greatest_number:
+#     #if number<greatest_number: #cp10_p5
+#         greatest_number = number
+# print(greatest_number)
+
+##################################################
+
+#cp10_p6:
+# number = int(input())
+# total = 0
+# seriesTotal=0
+# for i in range(0, number):  
+#     j = (10**i)*2
+#     total = total + j
+#     seriesTotal = seriesTotal+total
+# print(seriesTotal)
+
+#####################################################
+
+#cp10_p7&8:
+# number = int(input())
+# total = 0
+# for i in range(1, number+1):
+#     if (number%i == 0):
+#         #print(i)
+#         total = total+i
+# print(total)
+###############################################
+
+#cp10_p9
+# n = int(input())
+# total = 0
+# for i in range(1, n):
+#     if (n % i == 0):
+#         total = total + i
+# if total == n:
+#     print("Perfect Number")
+# else:
+#     print("Not a Perfect Number")
+
+###############################################
+
+#cp10_p10
+# n = int(input())
+# n = str(n)
+# total = 0
+# for i in n:
+#     i = int(i)
+#     j =(i**(len(n)))
+#     total = total + j
+# if total == int(n):
+#     print("Armstrong Number")
+# else:
+#     print("Not an Armstrong Number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
