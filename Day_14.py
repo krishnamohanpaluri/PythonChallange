0ca32c81c2ba1e91e9f0091b08853264d7c7f4a7
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-13 11:38:44 +0530

    Create Day_14.py

---
+++ Day_14.py
@@ -0,0 +1,34 @@
+######################Coding Practice_13############
+#p1&2 Prime Numbers from 1 to N
+
+ 
+# m = int(input()) 
+  
+# for i in range(1, m+1):
+#   if i>1:
+#     for j in range(2,i):
+#         if(i % j==0):
+#             break
+#     else:
+#         print(i)
+
+#############p3##########################
+# n = int(input())
+# for i in range(1, n+1):
+#     for j in range(1, i+1):
+#         print(str(j)+" ", end= '')
+#     print()
+
+#################p4#########################
+# n = int(input())
+# for i in range(0, n):
+#     for i in range(0, n-i):
+#         print(str(i+1)+" ", end ='')
+#     print()
+#################p5#########################
+# n = int(input())
+# for i in range(n):
+#     for row_num in range(i,n):
+#          print((str(n-row_num))+" ", end='')
+#     print()
+#############################################
