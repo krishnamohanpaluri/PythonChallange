d2fb4c5a101688eacb9c0416c67ee7afbadf95f8
krishnamohanpaluri <krishnamohan.mme@gmail.com>
2021-10-13 15:44:36 +0530

    Update Day_14.py

--- Day_14.py
+++ Day_14.py
@@ -32,3 +32,40 @@
 #          print((str(n-row_num))+" ", end='')
 #     print()
 #############################################
+# limit=int(input("Enter upper limit:"))
+# c=0
+# m=2
+# count = 0
+# while(c<limit):
+#     for n in range(1,m+1):
+#         a=m*m-n*n
+#         b=2*m*n
+#         c=m*m+n*n
+#         if(c>limit):
+#             break
+#         if(a==0 or b==0 or c==0):
+#             break
+#         count = count+1
+#     m=m+1
+# print(count)
+# from typing import Counter
+
+
+# n = int(input())
+# count = 0
+# for i in range(n+1):
+#     i = (i+1)**2
+#     for j in range(n-i):
+#         j = (j+1)**2
+#         for k in range(n):
+#             k = (k+1)**2
+#             if i + j == k:
+#                 count = count+1
+# print(count)
+################################################
+n = int(input())
+k = int(input())
+for i in range(k):
+    for j in range (n+2):   
+        print(str(n+j)+" ", end='')
+    print()
